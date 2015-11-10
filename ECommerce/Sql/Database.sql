f0e95ce9f6af303731c0e963cb30ef4aef4030b8
Hury Gabriel <hury_gabriel@hotmail.com>
2015-11-10 09:41:18 -0200

    up

--- ECommerce/Sql/Database.sql
+++ ECommerce/Sql/Database.sql
@@ -22,15 +22,45 @@ primary key(id)
 
 create table usuario(
 
-id
-email
-senha
+id int not null,
+email VARCHAR(320),
+senha varbinary(32),
 foreign key ( id ) references cliente(id)
+)
+--64 characters for the "local part" (username).
+--1 character for the @ symbol.
+--255 characters for the domain name.
+
+--SHA-256 has a 256 bit output
+--256bits = 32 Bytes
+--So try varbinary(32)
+
+create table pedido(
+
+id int not null,
+data datetime not null,
+valor decimal(7,2) not null
+primary key (id)
 
 )
 
-create table pedido()
+create table produto(
 
-create table produto()
+id int not null,
+nome varchar (100),
+valor decimal(7,2),
+descricao varchar(max),
+categoria int
+primary key(id)
 
-create table itempedido()
+)
+
+create table itempedido(
+valor decimal(7,2) not null
+quantidade int not null,
+idProduto int not null,
+idPedido int not null
+foreign key ( idProduto ) references produto(id)
+foreign key ( idPedido ) references pedido(id)
+
+)
