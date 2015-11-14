88a5b37bf8a1696643fd58c275768095751d6f1b
BlueTub <hury_gabriel@hotmail.com>
2015-11-14 17:47:38 -0200

    ManagedBean

---
+++ ECommerce/src/managedBean/UsuarioMB.java
@@ -0,0 +1,31 @@
+package managedBean;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+
+import model.Usuario;
+
+@ManagedBean
+public class UsuarioMB implements Serializable {
+	private static final long serialVersionUID = -7952903320250248386L;
+
+	public void adiciona(Usuario usu) {
+
+	}
+
+	public void altera() {
+
+	}
+
+	public List<Usuario> pesquisa(Usuario usu) {
+
+		return null;
+	}
+
+	public void exclui(Usuario usu) {
+
+	}
+
+}
--- ECommerce/src/model/Usuario.java
+++ ECommerce/src/model/Usuario.java
@@ -1,6 +1,11 @@
 package model;
 
-public class Usuario {
+import java.io.Serializable;
+
+
+
+
+public class Usuario implements Serializable {
 	
 	private int idUsuario;
 	private String email;
