7c483d9f019cc450ecbe57ed7f1f56d21a65244d
BlueTub <hury_gabriel@hotmail.com>
2015-11-18 10:46:27 -0200

    produto bean

--- ECommerce/src/managedBean/ProdutoMB.java
+++ ECommerce/src/managedBean/ProdutoMB.java
@@ -1,5 +1,48 @@
 package managedBean;
 
-public class ProdutoMB {
+import java.io.Serializable;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import model.Produto;
+import persistence.ProdutoDao;
+import persistence.ProdutoDaoImpl;
+
+@ManagedBean
+@SessionScoped
+public class ProdutoMB implements Serializable {
+	
+	private static final long serialVersionUID = 4196223195266927420L;
+
+	private Produto produto;
+	private ProdutoDao pDao;
+	
+	public ProdutoMB() {
+		produto = new Produto();
+		pDao = new ProdutoDaoImpl();
+	}
+	
+	void primeiraPagina(){
+		
+	}
+	
+	void carregaProdrutoCategoria(int categoria){
+		
+	}
+	
+	void caregaProdutoUnico(int id){
+		
+	}
+	
+	void pesquisaProduto(){
+		
+	}
+	
+	void icluiProduto(){
+		
+	}
+	
+	
+	
 }
